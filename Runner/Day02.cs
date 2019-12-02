1ddb160b9eb2b1fe654cea346feb5e85eed11a81
Paul Sweeney <github@kolossi.co.uk>
2019-12-02 08:36:56 +0000

    Day2 inc refactor interpreter to Intcode class after solution as it sounds like we are gonna need it

--- Runner/Day02.cs
+++ Runner/Day02.cs
@@ -9,17 +9,22 @@ namespace Runner
     {
         public override string First(string input)
         {
-            throw new NotImplementedException("First");
+            // not 337106 too low - read the question, reset data first!
+            int[] data = input.GetParts(",").Select(i => int.Parse(i)).ToArray();
+            int noun = 12;
+            int verb = 2;
+            return Intcode.Rerun(data, noun, verb)[0].ToString();
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException("Second");
+            int[] data = input.GetParts(",").Select(i => int.Parse(i)).ToArray();
+            return Solve(data, 19690720);
         }
 
         public override string FirstTest(string input)
         {
-            throw new NotImplementedException("FirstTest");
+            return Intcode.Execute(input.GetParts(",").Select(i => int.Parse(i)).ToArray())[0].ToString();
         }
 
         public override string SecondTest(string input)
@@ -28,5 +33,18 @@ namespace Runner
         }
 
         ////////////////////////////////////////////////////////
+
+        public string Solve(int[] data, int target)
+        {
+            for (int verb = 0; verb < 100; verb++)
+            {
+                for (int noun = 0; noun < 100; noun++)
+                {
+                    if (Intcode.Rerun(data, noun, verb)[0] == target)
+                        return string.Format("{0}{1}", noun, verb);
+                }
+            }
+            throw new InvalidOperationException("Huh?");
+        }
     }
 }
---
+++ Runner/Intcode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runner
+{
+    public class Intcode
+    {
+        public static int[] Rerun(int[] data, int noun, int verb)
+        {
+            int[] rerunData = new int[data.Length];
+            Array.Copy(data, rerunData, data.Length);
+            rerunData[1] = noun;
+            rerunData[2] = verb;
+            return Execute(rerunData);
+        }
+
+        public static int[] Execute(int[] data)
+        {
+            int ptr = 0;
+            while (true)
+            {
+                int opcode = data[ptr];
+                if (opcode == 99) break;
+                if (opcode != 1 && opcode != 2) throw new InvalidOperationException("Unknown opcode");
+                int a = data[data[ptr + 1]];
+                int b = data[data[ptr + 2]];
+                int storeAt = data[ptr + 3];
+                ptr += 4;
+                if (ptr >= data.Length) throw new IndexOutOfRangeException("ptr past end of data");
+                if (opcode == 1)
+                {
+                    data[storeAt] = a + b;
+                }
+                else if (opcode == 2)
+                {
+                    data[storeAt] = a * b;
+                }
+            }
+            return data;
+        }
+    }
+}
