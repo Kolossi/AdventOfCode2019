74d8f1ebe3fcf3edf3522e060ec253f877c8c106
paul <git@kolossi.co.uk>
2019-12-16 22:11:14 +0000

    Day 16 part 1

--- Runner/Day16.cs
+++ Runner/Day16.cs
@@ -9,18 +9,24 @@ namespace Runner
     {
         public override string First(string input)
         {
-            throw new NotImplementedException("First");
+            var parts = input.GetParts(",");
+            var phaseCount = int.Parse(parts[0]);
+            var fftInput = parts[1].ToCharArray().Select(c => long.Parse(c.ToString()));
+            var result = OperateFftPhases(fftInput, phaseCount);
+            return result;
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException("Second");
+            //spot zeros and skip inputs completely
+            // spot repeats and just sum x elements
+            // etc to optimise
         }
 
-        public override string FirstTest(string input)
-        {
-            throw new NotImplementedException("FirstTest");
-        }
+        //public override string FirstTest(string input)
+        //{
+        //    throw new NotImplementedException("FirstTest");
+        //}
 
         public override string SecondTest(string input)
         {
@@ -28,5 +34,39 @@ namespace Runner
         }
 
         ////////////////////////////////////////////////////////
+
+        private string OperateFftPhases(IEnumerable<long> fftInput, int phaseCount)
+        {
+            var fftBasePattern = new LinkedList<long>(new long[] { 0, 1, 0, -1 });
+            Queue<long> inputQueue = null;
+
+            for (int phase = 0; phase < phaseCount; phase++)
+            {
+                var output = new List<long>();
+                for (int outputDigit = 1; outputDigit <= fftInput.Count(); outputDigit++)
+                {
+                    var fftPattern = new LinkedList<long>();
+                    foreach (var patternDigit in fftBasePattern)
+                    {
+                        for (int i = 0; i < outputDigit; i++)
+                        {
+                            fftPattern.AddLast(patternDigit);
+                        }
+                    }
+                    var factor = fftPattern.First.Next;
+                    inputQueue = new Queue<long>(fftInput);
+                    long outputDigitValue = 0;
+                    for (int inputDigit = 0; inputDigit < fftInput.Count(); inputDigit++)
+                    {
+                        outputDigitValue += (factor.Value * inputQueue.Dequeue());
+                        factor = factor == fftPattern.Last ? fftPattern.First : factor.Next;
+                    }
+                    output.Add(Math.Abs(outputDigitValue) % 10);
+                }
+                fftInput = output;
+            }
+            return string.Join("", fftInput.Take(8));
+        }
+
     }
 }
